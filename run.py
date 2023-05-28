8eedeb83820ef0b426d848a334e4915daa04914f
Leost <2629105870@qq.com>
2023-05-28 20:01:09 +0800

    update

--- run.py
+++ run.py
@@ -11,6 +11,10 @@ def main():
     while not q.empty(): #队列不为空
         url_index=q.get() #拿到序号
 
+        if (url_index+1)%1000 ==0: #每进行1000轮保存一次数据
+            save_data(Team_number_list,Team_members_1_list,Team_members_2_list,Team_members_3_list,Instructor_list,
+              School_list,Awards_list)
+
         url=base_url+str(url_index)+'.pdf' #构造url
 
         #发送请求并转化成图片
@@ -33,10 +37,6 @@ def main():
             Instructor_list.append(faculty_name)
             School_list.append(school)
             Awards_list.append(awards)
-            if (url_index + 1) % 1000 == 0:  # 每进行1000轮保存一次数据
-                save_data(Team_number_list, Team_members_1_list, Team_members_2_list, Team_members_3_list,
-                          Instructor_list,
-                          School_list, Awards_list)
 
             #释放锁
             muti_lock.release()
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     try:
         with open('config.yaml','r',encoding='utf-8') as f:
             config=yaml.safe_load(f)
-            n=config['n'] #总共爬取的队伍数量（设置较大的一个数）
+            index=list(filter(None,config['index'].split('-'))) #总共爬取的队伍序号范围，列表形式['1','24000']
             n_thread=config['n_thread'] #线程数量
         try:
             tessdata=config['tessdata'] #语言包路径
@@ -89,18 +89,19 @@ if __name__ == '__main__':
     # 4进行匹配奖项
     awards_re = re.compile(r'Was Designated As(.*)\n+.+',re.I | re.S | re.DOTALL)
     #创建队列
+    n=int(index[1])-int(index[0])+1 #爬取的总数量
     q=queue.Queue(maxsize=n)
+    #将url数据存入队列中
 
-    #将url数据存入队列中  （注意2301434这种导师名称带中文，学校也带中文的会报错）
-    for i in range(n):
+    for i in range(int(index[0]),int(index[1])):
         q.put(2300000+i)
 
     # 下面创建一个线程锁对象，防止多线程存入结果中发生错乱（通过后续调用acquire和release方法来使用）,全局变量用于main()
     muti_lock = threading.Lock()
 
     # 创建存储列表
-    Team_number_list= [] #存储队伍号
-    Team_members_1_list= [] #队员1
+    Team_number_list=[] #存储队伍号
+    Team_members_1_list=[] #队员1
     Team_members_2_list = [] #队员2
     Team_members_3_list = [] #队员3
     Instructor_list=[] #指导老师
